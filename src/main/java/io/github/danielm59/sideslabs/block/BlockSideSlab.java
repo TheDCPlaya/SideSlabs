96dd41971c3eb86fb9adc9c24fb655c5d8b4206c
danielm59 <danielm59@gmail.com>
2016-11-22 12:48:38 +0000

    +added hardness and resistance to blocks

--- src/main/java/io/github/danielm59/sideslabs/block/BlockSideSlab.java
+++ src/main/java/io/github/danielm59/sideslabs/block/BlockSideSlab.java
@@ -15,136 +15,139 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class BlockSideSlab extends Block 
+public class BlockSideSlab extends Block
 {
 
-	public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
-	
-	 protected static final AxisAlignedBB AABB_NORTH = new AxisAlignedBB(0.0D, 0.0D, 0.5D, 1.0D, 1.0D, 1.0D);
-	 protected static final AxisAlignedBB AABB_EAST = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 0.5D, 1.0D, 1.0D);
-	 protected static final AxisAlignedBB AABB_SOUTH = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 0.5D);
-	 protected static final AxisAlignedBB AABB_WEST = new AxisAlignedBB(0.5D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
-	 
-	public BlockSideSlab(Material material) {
-		super(material);
-		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-		this.setCreativeTab(CreativeTabSS.SIDESLABS_TAB);
-	}
-	
-	@Override
-	public void onBlockAdded(World worldIn, BlockPos p, IBlockState state)
-	{
-		if (!worldIn.isRemote)
-		{
-			IBlockState block = worldIn.getBlockState(p.north());
-			IBlockState block1 = worldIn.getBlockState(p.south());
-			IBlockState block2 = worldIn.getBlockState(p.west());
-			IBlockState block3 = worldIn.getBlockState(p.east());
-			EnumFacing enumfacing = state.getValue(FACING);
-			if (enumfacing == EnumFacing.NORTH && block.isFullBlock() && !block1.isFullBlock())
-			{
-				enumfacing = EnumFacing.SOUTH;
-			} else if (enumfacing == EnumFacing.SOUTH && block1.isFullBlock() && !block.isFullBlock())
-			{
-				enumfacing = EnumFacing.NORTH;
-			} else if (enumfacing == EnumFacing.WEST && block2.isFullBlock() && !block3.isFullBlock())
-			{
-				enumfacing = EnumFacing.EAST;
-			} else if (enumfacing == EnumFacing.EAST && block3.isFullBlock() && !block2.isFullBlock())
-			{
-				enumfacing = EnumFacing.WEST;
-			}
-			worldIn.setBlockState(p, state.withProperty(FACING, enumfacing), 2);
-		}
-	}
-	
-	@Override
-	public IBlockState getStateFromMeta(int meta)
-	{
-		EnumFacing enumfacing = EnumFacing.getFront(meta);
-		if (enumfacing.getAxis() == EnumFacing.Axis.Y)
-		{
-			enumfacing = EnumFacing.NORTH;
-		}
-		return this.getDefaultState().withProperty(FACING, enumfacing);
-	}
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 
-	@Override
-	public int getMetaFromState(IBlockState state)
-	{
-		return state.getValue(FACING).getIndex();
-	}
+    protected static final AxisAlignedBB AABB_NORTH = new AxisAlignedBB(0.0D, 0.0D, 0.5D, 1.0D, 1.0D, 1.0D);
+    protected static final AxisAlignedBB AABB_EAST = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 0.5D, 1.0D, 1.0D);
+    protected static final AxisAlignedBB AABB_SOUTH = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 0.5D);
+    protected static final AxisAlignedBB AABB_WEST = new AxisAlignedBB(0.5D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
 
-	@Override
-	protected BlockStateContainer createBlockState()
-	{
-		return new BlockStateContainer(this, new IProperty[]{ FACING });
-	}
-
-	@Override
-	public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
-	{
-		boolean flag1 = hitX + hitZ > 1.0f;
-		boolean flag2 = hitX > hitZ;
-		
-		EnumFacing blockFacing = EnumFacing.NORTH;
-		if (flag1)
-		{
-			if (flag2)
-			{
-				blockFacing=EnumFacing.WEST;
-			}else
-			{
-				blockFacing=EnumFacing.NORTH;
-			}
-		}else
-		{
-			if (flag2)
-			{
-				blockFacing=EnumFacing.SOUTH;
-			}else
-			{
-				blockFacing=EnumFacing.EAST;
-			}
-		}
-		
-		return this.getDefaultState().withProperty(FACING, blockFacing);
-	}
-
-	@Override
-	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
-	{
-		worldIn.setBlockState(pos, state, 2);
-	}
-	
-	@Override
-	public boolean isOpaqueCube(IBlockState state)
-	{
-		return false;
-	}
-
-	@Override
-	public boolean isFullCube(IBlockState state)
-	{
-		return false;
-	}
-	
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    public BlockSideSlab(Material material)
     {
-		switch(state.getValue(FACING))
-		{
-		case EAST:
-			return AABB_EAST;
-		case NORTH:
-			return AABB_NORTH;
-		case SOUTH:
-			return AABB_SOUTH;
-		case WEST:
-			return AABB_WEST;
-		default:
-			return FULL_BLOCK_AABB;
-		}
-		
+        super(material);
+        this.setHardness(2.0f);
+        this.setResistance(material == Material.WOOD ? 5 : 10);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+        this.setCreativeTab(CreativeTabSS.SIDESLABS_TAB);
+    }
+
+    @Override
+    public void onBlockAdded(World worldIn, BlockPos p, IBlockState state)
+    {
+        if (!worldIn.isRemote)
+        {
+            IBlockState block = worldIn.getBlockState(p.north());
+            IBlockState block1 = worldIn.getBlockState(p.south());
+            IBlockState block2 = worldIn.getBlockState(p.west());
+            IBlockState block3 = worldIn.getBlockState(p.east());
+            EnumFacing enumfacing = state.getValue(FACING);
+            if (enumfacing == EnumFacing.NORTH && block.isFullBlock() && !block1.isFullBlock())
+            {
+                enumfacing = EnumFacing.SOUTH;
+            } else if (enumfacing == EnumFacing.SOUTH && block1.isFullBlock() && !block.isFullBlock())
+            {
+                enumfacing = EnumFacing.NORTH;
+            } else if (enumfacing == EnumFacing.WEST && block2.isFullBlock() && !block3.isFullBlock())
+            {
+                enumfacing = EnumFacing.EAST;
+            } else if (enumfacing == EnumFacing.EAST && block3.isFullBlock() && !block2.isFullBlock())
+            {
+                enumfacing = EnumFacing.WEST;
+            }
+            worldIn.setBlockState(p, state.withProperty(FACING, enumfacing), 2);
+        }
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        EnumFacing enumfacing = EnumFacing.getFront(meta);
+        if (enumfacing.getAxis() == EnumFacing.Axis.Y)
+        {
+            enumfacing = EnumFacing.NORTH;
+        }
+        return this.getDefaultState().withProperty(FACING, enumfacing);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        return state.getValue(FACING).getIndex();
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[]{FACING});
+    }
+
+    @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        boolean flag1 = hitX + hitZ > 1.0f;
+        boolean flag2 = hitX > hitZ;
+
+        EnumFacing blockFacing = EnumFacing.NORTH;
+        if (flag1)
+        {
+            if (flag2)
+            {
+                blockFacing = EnumFacing.WEST;
+            } else
+            {
+                blockFacing = EnumFacing.NORTH;
+            }
+        } else
+        {
+            if (flag2)
+            {
+                blockFacing = EnumFacing.SOUTH;
+            } else
+            {
+                blockFacing = EnumFacing.EAST;
+            }
+        }
+
+        return this.getDefaultState().withProperty(FACING, blockFacing);
+    }
+
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+    {
+        worldIn.setBlockState(pos, state, 2);
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        switch (state.getValue(FACING))
+        {
+            case EAST:
+                return AABB_EAST;
+            case NORTH:
+                return AABB_NORTH;
+            case SOUTH:
+                return AABB_SOUTH;
+            case WEST:
+                return AABB_WEST;
+            default:
+                return FULL_BLOCK_AABB;
+        }
+
     }
 
 }
